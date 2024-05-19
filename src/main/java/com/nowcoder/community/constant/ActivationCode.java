406d709c7768306277464e11374f64195272bf0b
Smilexing <2664626327@qq.com>
2024-05-19 17:06:45 +0800

    论坛平台community-完善注册逻辑，可以跳转页面

---
+++ src/main/java/com/nowcoder/community/constant/ActivationCode.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community.constant;
+
+public interface ActivationCode {
+
+    /**
+     * 成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+    /**
+     * 重复
+     */
+
+    int ACTIVATION_REPEAT = 1;
+
+    /**
+     * 失败
+     */
+
+    int ACTIVATION_FAILURE = 2;
+
+}
---
+++ src/main/java/com/nowcoder/community/utils/CommunityUtil.java
@@ -0,0 +1,34 @@
+package com.nowcoder.community.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+/**
+ * @author Tom Smile
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/5/19 12:24
+ */
+
+public class CommunityUtil {
+    /**
+     * 生成随机字符串
+     * @return
+     */
+    public static String generateUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    /**
+     * MD5加密（传入盐值）
+     */
+
+    public static String md5(String key) {
+        if (StringUtils.isBlank(key)) {
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+}
